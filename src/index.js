3145aacc44f199ca30304b29f737cc1e7d6f084e
Mariana <marianabmoraes.ef@gmail.com>
2024-08-19 09:29:09 -0300

    feat: add routes & init server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-let obj = {
-    name: "baralho",
-}
+const express = require("express");
+const naipe_controller = require("./controllers/naipe.js");
+const app = express();
+const port = 3000;
+
+app.post("/naipe", (req, res) => {
+  const naipe = req.body;
+  const code = naipe_controller.store(naipe);
+  res.status(code).json();
+});
+
+app.get("./naipe", (req, res) => {
+  const naipes = naipe_controller.idex();
+  res.json(naipes);
+});
+
+app.get("./naipe/:id", (req, res) => {
+  const naipe = naipe_controller.show(req.params.id);
+  res.json(naipe);
+});
+
+app.put("./naipe/:id", (req, res) => {
+  const naipe = req.body;
+  const code = naipe_controller.update(req.params.id, naipe);
+  res.status(code).json();
+});
+
+app.delete("/naipe/:id", (req, res) => {
+  naipe_controller.destroy(req.params.id);
+  res.json();
+});
+
+app.listen(port, () => {
+  console.log("Gerenciador de baralho executando na porta: " + port);
+});
